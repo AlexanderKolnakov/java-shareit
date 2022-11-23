440c9683d9c0a6477d5bf06b6e9d8e0fbb81e9a7
AlexanderKolnakov <akolmakov1994@gmail.com>
2022-11-23 19:23:03 +0300

    code style 3

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -1,9 +1,11 @@
 package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class ShareItApp {
 
 	public static void main(String[] args) {
