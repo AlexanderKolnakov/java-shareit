2c35ccb81409dccbab16a83fb33ea011ffe3cb8d
AlexanderKolnakov <akolmakov1994@gmail.com>
2023-02-24 16:02:35 +0300

    -git check style

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -21,7 +21,6 @@ import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
